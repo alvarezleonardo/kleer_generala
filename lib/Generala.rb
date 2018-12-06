5a9363739553d03d1e91c889c86e5ffb3ffbe164
vagrant <vagrant@kleer-csd-box>
2018-12-06 15:16:58 -0300

    Clase generada con metodo tirarDados, retorna vector numerico de 5

---
+++ lib/Generala.rb
@@ -0,0 +1,14 @@
+class Generala
+	def initialize 
+		@dados = [1, 2, 3, 4, 5]
+		@puntos = 0
+	end
+	def getDados
+		return @dados
+	end
+
+	def tirarDados
+		return getDados
+	end
+	
+end
---
+++ spec/generala_spec.rb
@@ -0,0 +1,9 @@
+require "./lib/Generala.rb"
+
+describe "Partido Generala" do
+	it "Tirada de dados para generala" do
+		juego = Generala.new
+		expect( juego.tirarDados ).to eq [1,2,3,4,5] 
+	end
+
+end
