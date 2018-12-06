728a1407ce6b289b6493e3cd552df157b52970bc
Leonardo Alvarez <leonardo.luis.alvarez@gmail.com>
2018-12-06 15:57:16 -0300

    incluimos función getIsPoker true o false, metodo para probar array y dados random

--- lib/Generala.rb
+++ lib/Generala.rb
@@ -4,11 +4,28 @@ class Generala
 		@puntos = 0
 	end
 	def getDados
+		@dados = [rand(6), rand(6), rand(6), rand(6), rand(6)]
 		return @dados
 	end
 
 	def tirarDados
 		return getDados
+	end
+	def getDadosOrdenados
+		return @dados.sort 
+	end
+
+	def setDados dadosPrueba
+		@dados = dadosPrueba
+	end
+
+	def getIsPoker
+		ok = false		
+		dadosOrd = getDadosOrdenados
+		if dadosOrd[0] != dadosOrd[4] and (dadosOrd[0] == dadosOrd[3] or dadosOrd[4] == dadosOrd[3])
+			ok = true
+		end
+		return ok
 	end
 	
 end
--- spec/generala_spec.rb
+++ spec/generala_spec.rb
@@ -3,7 +3,20 @@ require "./lib/Generala.rb"
 describe "Partido Generala" do
 	it "Tirada de dados para generala" do
 		juego = Generala.new
-		expect( juego.tirarDados ).to eq [1,2,3,4,5] 
+		expect( juego.tirarDados ).to be_an_instance_of(Array) 
+		expect( juego.tirarDados.length ).to eq 5 
+	end
+
+	it "Tirada de dados y verificar si da poker" do
+		juego = Generala.new
+		juego.setDados [1, 2, 2, 2, 2]
+		expect( juego.getIsPoker ).to eq true
+	end
+
+	it "Tirada de dados y verificar que no es poker" do
+		juego = Generala.new
+		juego.setDados [1, 2, 2, 2, 3]
+		expect( juego.getIsPoker ).to eq false
 	end
 
 end
