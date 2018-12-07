532b5c1e66173fa5437ec740b111919f7c721268
vagrant <vagrant@kleer-csd-box>
2018-12-07 10:20:32 -0300

    Agregar puntajes

--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -14,4 +14,10 @@ When(/^jugador tira dados$/) do
   click_button("Tirar dados")
 end
 
+When(/^Tira dados y sale (\d+) (\d+) (\d+) (\d+) (\d+)$/) do |arg1, arg2, arg3, arg4, arg5|
+  @@generala.setDados [arg1, arg2, arg3, arg4, arg5]
+  click_button("Tirar dados")
+end
+
+
 
